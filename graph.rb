9fd0851c8a7492684a0de1867b7544e59a5154e2
Tim Adler <tim@emrys-merlin.de>
2017-02-01 12:17:20 +0100

    Refactor graph class by rubocop

--- graph.rb
+++ graph.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
 require_relative 'matrix.rb'
 require_relative 'circle.rb'
 
+# This class contains part of the Caley graph of a group of moebius transformations.
 class Graph
   include Enumerable
   attr_reader :depth, :root, :gen
-  
+
   def initialize(p, gen, depth)
-    @root = Node.new(p, Matrix[[1,0], [0, 1]], Matrix[[1,0], [0,1]], 0, [], [])
+    @root = Node.new(p, Matrix[[1, 0], [0, 1]], Matrix[[1, 0], [0, 1]],
+                     0, [], [])
     @depth = 0
-    @gen = gen.map do |g|
-      g.proj_inv
-    end
+    @gen = gen.map(&:proj_inv)
     @gen += gen
     deepen(depth)
   end
@@ -18,15 +19,15 @@ class Graph
   def deepen(depth, eps = 0.001)
     stack = [@root]
 
-    until stack.empty? do
+    until stack.empty?
       node = stack.pop
 
       next if node.depth >= depth
-      
+
       if node.depth >= @depth
         @gen.each do |g|
           next if g.proj_inv == node.prefix
-          
+
           q = g.moebius(node.p)
 
           skip = false
@@ -38,7 +39,7 @@ class Graph
             end
           end
 
-          node.children << Node.new(q, g*node.g, g, node.depth+1, [], [])
+          node.children << Node.new(q, g * node.g, g, node.depth + 1, [], [])
         end
       end
 
@@ -61,12 +62,12 @@ class Graph
       lc = []
       circles.each do |c|
         i = c.triple_inclusion(center.p, circ)
-        if i == 0
+        if i.zero?
           lc << c
         elsif i == 1
           lc << c
           admissible = false
-          break
+          # break
         end
       end
       lc << circ if admissible
@@ -77,7 +78,7 @@ class Graph
 
   # Translate the Dirichlet fundamental domain around center to all
   # its orbit points (up to depth)
-  def get_tesselation(center = @root)
+  def get_tessellation(center = @root)
     get_intersection(center)
 
     each do |node|
@@ -88,15 +89,23 @@ class Graph
       end
     end
   end
-  
+
   # a pre-order depth first search implementation to visit all nodes
   def each
-    stack = [@root]
-    until stack.empty? do
-      node = stack.pop
-      stack += node.children
+    queue = [@root]
+    until queue.empty?
+      node = queue.shift
+      queue += node.children
       yield node
     end
+  end
+
+  def nodes
+    nodes = []
+    each do |node|
+      nodes << node
+    end
+    nodes
   end
 end
 
